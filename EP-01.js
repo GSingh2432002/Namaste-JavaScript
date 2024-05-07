7fff81994b0a9b667f241b64f4555842de7ecf8e
GSingh2432002 <gsingh332211@gmail.com>
2024-05-07 09:38:24 +0530

    Update

---
+++ EP-01.js
@@ -0,0 +1,11 @@
+`Execution Context`
+
+/*Everything in JavaScript happens inside an Execution Context:-*/
+
+/* 
+Execution Context divided into two parts:-
+1. Variable Environment aka Memory in which data is stored in Key:Value pair or as function.
+2. Thread of Execution aka Code in which code is executed one line at a time.
+*/
+
+// JavaScript is a synchronous single-threaded language.
