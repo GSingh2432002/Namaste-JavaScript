5ec24f21d5f6524264674415b32b02a92929902d
GSingh2432002 <gsingh332211@gmail.com>
2024-05-11 23:23:21 +0530

    Update

---
+++ EP-15.js
@@ -0,0 +1,4 @@
+`JS engine`
+ /*
+JavaScript Runtime Enviroment -> Browser/Node.js -> JS Engine/API/MQ/CQ -> Inside JS Engine -> Code is going through Parsing-> code convert into Tokens->It forms AST(Abstract Syntax Tree) ->Compilation->Interpreter/Compiler(i.e it is known as JIT or Just In Time Compilation)->Execution->Then interpreter produces the byte code that is executed with help of Memory Heap and Call Stack.
+*/  
